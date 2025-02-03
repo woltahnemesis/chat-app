a9e00a76233ba27fd81bb5075efca79fee48543c
Rey Magbanua <alucardmagbanua@gmail.com>
2025-02-03 03:49:47 -0500

    Update encoding to UTF-8 Procfile

--- app/app.js
+++ app/app.js
@@ -1,4 +1,5 @@
 // const socket = io('ws://localhost:8080')
+// const socket = io('ws://localhost:5006')
 const socket = io('wss://chat-web-app-f71757d69e07.herokuapp.com');
 
 // Display message on browser
